073706d9fffa1dda7ac3519f4ed5dafd2fdc00e1
laylabrassington <134307970+laylabrassington@users.noreply.github.com>
2026-09-04 14:53:24 -0500

    Update merge_filter_2.sh

--- scripts/merge_filter_2.sh
+++ scripts/merge_filter_2.sh
@@ -2,7 +2,7 @@
 #SBATCH --mail-user=your_email@university.edu
 #SBATCH --mail-type=ALL
 #SBATCH -J merge_filter2
-#SBATCH --time=08:00:00
+#SBATCH --time=04:00:00
 #SBATCH --mem=16GB
 #SBATCH --output=merge_filter2.out
 
